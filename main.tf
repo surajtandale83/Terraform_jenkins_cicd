9ad0bfb25fe6f3b59e2e5d2016869e505b51b54c
Surajtandale <suraj.tandale@gmail.com>
2025-03-04 15:19:02 +0530

    new code add

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "terrafrom" {
+    ami =  "ami-0d682f26195e9ec0f"
+    instance_type = "t2.micro"
+    key_name = "multicloud"
+    tags =  {
+           name ="dev2"
+    }
+      
+}
